beacfd4357803904c787c83c5bda6c3dd983a736
yoanPTDev <yoan.pt.dev@outlook.com>
2023-04-12 14:08:41 -0400

    retirer des tabs

--- Partie1/transition.py
+++ Partie1/transition.py
@@ -44,10 +44,10 @@ class Transition(ABC):
 class ConditionalTransition(Transition):
     def __init__(self, next_state: Optional[State] = None, condition: Optional[Condition] = None):
         super().__init__(next_state)
-        
+
         if not isinstance(condition, (Condition, NoneType)):
             raise TypeError('condition must be of type Condition')
-        
+
         self.__condition: Optional[Condition] = condition
 
     def is_valid(self) -> bool:
@@ -70,11 +70,11 @@ class ActionTransition(ConditionalTransition):
     def __init__(self, next_state: Optional[State] = None, condition: Optional[Condition] = None):
         super().__init__(next_state, condition)
         self.__transiting_actions: list[Callable[[], None]] = []
-        
+
     def _do_transiting_action(self):
         for action in self.__transiting_actions:
             action()
-    
+
     def add_transition_action(self, action: Callable[[], None]) -> None:
         self.__transiting_actions.append(action)
 
@@ -85,21 +85,21 @@ class MonitoredTransition(ActionTransition):
         self.__transit_count = 0
         self.__last_transit_time = 0.
         self.custom_value = None
-        
+
     @property
     def transit_count(self) -> int:
         return self.__transit_count
-    
+
     @property
     def last_transit_time(self) -> float:
         return self.__last_transit_time
-    
+
     def reset_transit_count(self) -> None:
         self.__transit_count = 0
-        
+
     def reset_last_transit_time(self) -> None:
         self.__last_transit_time = 0.
-        
+
     def _exec_transiting_action(self):
         self.__transit_count += 1
         self.__last_transit_time = time.perf_counter()
