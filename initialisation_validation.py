b3c4334bb1a06520b97dd0837a7d82c05ea16ff3
Venti Bear <n_drolet@hotmail.ca>
2023-05-03 13:46:02 -0400

    Creating initialisation_validation.py to keep C64Projet clean

---
+++ initialisation_validation.py
@@ -0,0 +1,10 @@
+from state import MonitoredState
+from layout import Layout
+from transition import ConditionalTransition
+from condition import StateEntryDurationCondition
+from Robot import Robot
+
+class InitializationValidation(Layout):
+    def __init__(self):
+
+
