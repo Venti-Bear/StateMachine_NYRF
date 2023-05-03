9f2e81d0a2a3a0ee4ad8fedf47ee44959d57eb08
Venti Bear <n_drolet@hotmail.ca>
2023-05-03 14:21:36 -0400

    more work on InitValid

--- initialisation_validation.py
+++ initialisation_validation.py
@@ -1,4 +1,4 @@
-from state import MonitoredState
+from state import *
 from layout import Layout
 from transition import ConditionalTransition
 from condition import StateEntryDurationCondition
@@ -6,5 +6,15 @@ from Robot import Robot
 
 class InitializationValidation(Layout):
     def __init__(self):
+        self.__robot = Robot()
 
+        ROBOT_INSTANTIATION = MonitoredState()
+
+        INSTANTIATION_FAILED = ActionState()
+        INSTANTIATION_FAILED.add_in_state_action(lambda: (print("Robot is not connected")))
+
+        ROBOT_INTEGRITY = MonitoredState()
+        ROBOT_INTEGRITY.add_in_state_action(self.__robot.check_integrity)
+        
+        END = State(Parameters(False, True, True))
 
