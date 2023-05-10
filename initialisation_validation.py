e214e61bb8a34817bdd2264812f228d89d2ece03
Venti Bear <n_drolet@hotmail.ca>
2023-05-10 15:02:46 -0400

    states for validation are done. Transitions and conditions remain.

--- initialisation_validation.py
+++ initialisation_validation.py
@@ -3,6 +3,7 @@ from layout import Layout
 from transition import ConditionalTransition
 from condition import StateEntryDurationCondition
 from Robot import Robot
+from blinker import Side
 
 class InitializationValidation(Layout):
     def __init__(self):
@@ -16,5 +17,18 @@ class InitializationValidation(Layout):
         ROBOT_INTEGRITY = MonitoredState()
         ROBOT_INTEGRITY.add_in_state_action(self.__robot.check_integrity)
         
-        END = State(Parameters(False, True, True))
+        END = ActionState(Parameters(False, True, True))
+        END.add_in_state_action(lambda: (print("Robot shutting down")))
+        
+        INTEGRITY_FAILED = ActionState(Parameters(False, True, True))
+        INTEGRITY_FAILED.add_in_state_action(lambda: (print('One or more components are not working')))
+        INTEGRITY_FAILED.add_in_state_action(self.__robot.set_eye_color("red"))
+        INTEGRITY_FAILED.add_in_state_action(self.__robot.blink(Side.BOTH, total_duration=5.0, cycle_duration=0.5, percent_on=0.5))
+
+        INTEGRITY_SUCCEEDED = MonitoredState()
+        INTEGRITY_SUCCEEDED.add_in_state_action(lambda: (print('Initialization successful, starting robot')))
+        INTEGRITY_SUCCEEDED.add_in_state_action(self.__robot.set_eye_color("green"))
+        INTEGRITY_SUCCEEDED.add_in_state_action(self.__robot.blink(Side.BOTH, total_duration=3.0, cycle_duration=1.0, percent_on=0.5))
+
+
 
