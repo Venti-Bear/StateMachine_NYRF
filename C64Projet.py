568350c189d7d43fefe8bae00041ff242f24531a
yoanPTDev <yoan.pt.dev@outlook.com>
2023-05-18 08:51:08 -0400

    Commentaires retiré

--- C64Projet.py
+++ C64Projet.py
@@ -1,19 +1,3 @@
-# INITIALISATION ET VALIDATION
-# ============================
-# ROBOT_INSTANTIATION -> ROBOT_INTEGRITY if successful else INSTANTIATION_FAILED -> END
-# ROBOT_INTEGRITY -> INTEGRITY SUCCEEDED if successful else INTEGRITY_FAILED -> SHUT_DOWN_ROBOT -> END
-# INTEGRITY_SUCCEEDED -> HOME 
-
-# ACCEUIL
-# ============================
-# HOME - TACHES (TASK_1, TASK_2, TASK_3, ...)
-
-# TACHE
-# ============================
-# TASK_1 -> HOME
-# TASK_2 -> HOME
-# ...
-
 from finite_state_machine import FiniteStateMachine
 from c64_layout import C64Layout
 
--- Task01_manual_control.py
+++ Task01_manual_control.py
@@ -1,3 +1,5 @@
+from turtle import back
+from blinker import Side
 from transition import ConditionalTransition
 from condition import StateValueCondition
 from finite_state_machine import FiniteStateMachine
@@ -5,10 +7,10 @@ from robot_state import RobotState
 from layout import Layout
 from Robot import Robot, Direction
 
+
 class ManualControlState(RobotState):
     def STOP(self):
         self.robot.direction = None
-        
 
     def __init__(self, robot: Robot):
         super().__init__(robot)
@@ -18,49 +20,69 @@ class ManualControlState(RobotState):
         self.add_in_state_action(lambda: self.__manual_control.track())
         self.add_exiting_action(self.STOP)
 
+
 class ManualControl(FiniteStateMachine):
     def __init__(self, robot: Robot):
         self.robot = robot
-        FORWARD, BACKWARD, ROTATE_LEFT, ROTATE_RIGHT, STOP = RobotState(self.robot), RobotState(self.robot), RobotState(self.robot), RobotState(self.robot), RobotState(self.robot)
+        FORWARD, BACKWARD, ROTATE_LEFT, ROTATE_RIGHT, STOP = [RobotState(self.robot) for _ in range(5)]
+
+        left_condition, not_left_condition = StateValueCondition(
+            'left', STOP), StateValueCondition('left', ROTATE_LEFT, inverse=True)
+        left, not_left = ConditionalTransition(
+            ROTATE_LEFT, left_condition), ConditionalTransition(STOP, not_left_condition)
         
-        right_condition, not_right_condition = StateValueCondition('right', STOP), StateValueCondition('right', ROTATE_RIGHT, inverse=True)
-        left_condition, not_left_condition = StateValueCondition('left', STOP), StateValueCondition('left', ROTATE_LEFT, inverse=True)
-        up_condition, not_up_condition = StateValueCondition('up', STOP), StateValueCondition('up', FORWARD, inverse=True)
-        down_condition, not_down_condition = StateValueCondition('down', STOP), StateValueCondition('down', BACKWARD, inverse=True)
-
-        right, not_right = ConditionalTransition(ROTATE_RIGHT, right_condition), ConditionalTransition(STOP, not_right_condition)
-        left, not_left = ConditionalTransition(ROTATE_LEFT, left_condition), ConditionalTransition(STOP, not_left_condition)
-        up, not_up = ConditionalTransition(FORWARD, up_condition), ConditionalTransition(STOP, not_up_condition)
-        down, not_down = ConditionalTransition(BACKWARD, down_condition), ConditionalTransition(STOP, not_down_condition)
-
-        FORWARD.add_transition(not_up), STOP.add_transition(up)
-        BACKWARD.add_transition(not_down), STOP.add_transition(down)
-        ROTATE_RIGHT.add_transition(not_right), STOP.add_transition(right)
         ROTATE_LEFT.add_transition(not_left), STOP.add_transition(left)
 
+        right, not_right = ConditionalTransition(
+            ROTATE_RIGHT, right_condition), ConditionalTransition(STOP, not_right_condition)
+        right_condition, not_right_condition = StateValueCondition(
+            'right', STOP), StateValueCondition('right', ROTATE_RIGHT, inverse=True)
+        
+        ROTATE_RIGHT.add_transition(not_right), STOP.add_transition(right)
+        
+        down_condition, not_down_condition = StateValueCondition(
+            'down', STOP), StateValueCondition('down', BACKWARD, inverse=True)
+        down, not_down = ConditionalTransition(
+            BACKWARD, down_condition), ConditionalTransition(STOP, not_down_condition)
+        
+        BACKWARD.add_transition(not_down), STOP.add_transition(down)
+
+        up_condition, not_up_condition = StateValueCondition(
+            'up', STOP), StateValueCondition('up', FORWARD, inverse=True)
+        up, not_up = ConditionalTransition(
+            FORWARD, up_condition), ConditionalTransition(STOP, not_up_condition)
+        
+        FORWARD.add_transition(not_up), STOP.add_transition(up)
+        
         def forward():
             robot.movement_direction = Direction.FORWARD
 
         def backward():
             robot.movement_direction = Direction.BACKWARD
 
-        def right():
+        def rotate_right():
             robot.movement_direction = Direction.RIGHT
 
-        def left():
+        def rotate_left():
             robot.movement_direction = Direction.LEFT
 
         def stop():
             robot.movement_direction = None
 
         FORWARD.add_in_state_action(forward)
+        FORWARD.add_entering_action(lambda: robot.blink_led(Side.BOTH, cycle_duration=1.0, percent_on=0.25, begin_on=True, end_off=False))
         BACKWARD.add_in_state_action(backward)
-        ROTATE_RIGHT.add_in_state_action(right)
-        ROTATE_LEFT.add_in_state_action(left)
+        BACKWARD.add_entering_action(lambda: robot.blink_led(Side.BOTH, cycle_duration=1.0, percent_on=0.75, begin_on=True, end_off=False))
+        ROTATE_RIGHT.add_in_state_action(rotate_right)
+        ROTATE_RIGHT.add_entering_action(lambda: robot.blink_led(Side.RIGHT, cycle_duration=1.0, percent_on=0.50, begin_on=True, end_off=False))
+        ROTATE_LEFT.add_in_state_action(rotate_left)
+        ROTATE_LEFT.add_entering_action(lambda: robot.blink_led(Side.LEFT, cycle_duration=1.0, percent_on=0.50, begin_on=True, end_off=False))
         STOP.add_in_state_action(stop)
-        
+        STOP.add_entering_action(lambda: robot.turn_led_off(side=Side.BOTH))
+
         self.__layout = Layout()
-        self.__layout.add_states({ FORWARD, BACKWARD, ROTATE_LEFT, ROTATE_RIGHT, STOP })
+        self.__layout.add_states(
+            {FORWARD, BACKWARD, ROTATE_LEFT, ROTATE_RIGHT, STOP})
         self.__layout.initial_state = STOP
 
         super().__init__(self.__layout, uninitialized=False)
